d892a80b44f72926522160ef7be3c005aae5d238
Babor14 <145376985+Babor14@users.noreply.github.com>
2024-12-02 20:26:55 +0300

    Update md1.py

--- md1.py
+++ md1.py
@@ -1,9 +1,9 @@
 import random
 
-# Шаг 1: Заполнение файла f случайными целыми числами
+
 with open('f.txt', 'w') as f:
-    for _ in range(100):  # Генерируем 100 случайных чисел
-        number = random.randint(1, 100)  # Случайное число от 1 до 100
+    for _ in range(100):  
+        number = random.randint(1, 100)  
         f.write(f"{number}\n")
 
 
